81816d3f71b8cedbd36c8c8ba0d7b5dac440f231
Agata-Duda <agaiduda@gmail.com>
2022-09-30 10:04:21 +0100

    added input fields for user

--- src/SignupForm.js
+++ src/SignupForm.js
@@ -13,12 +13,24 @@ const SignupForm = () => {
         <Collapsible trigger={<b>Sign up</b>} triggerWhenOpen={<b>Sign up</b>}>
             <div id="formSignup" className="SignupForm">
                     <form onSubmit={handleSubmit(onSubmit)}>
-                        <input id="SignupUsername" defaultValue={"Username"} {...register("Username", { required: true})} />
-                        <br/>
                         <input id="SignupName" defaultValue={"First Name"} {...register("FirstName", { required: true})} />
                         <br/>
                         <input id="SignupLastname" defaultValue={"Last Name"} {...register("LastName", { required: true})} />
                         {errors.loginRequired && <span> Password is required</span>}
+                        <br/>
+                        <input id="SignupUsername" defaultValue={"Username"} {...register("Username", { required: true})} />
+                        <br/>
+                        <input id="SignupEmail" defaultValue={"Email"} {...register("Email", { required: true})} />
+                        {errors.loginRequired && <span> Password is required</span>}
+                        <br/>
+                        <select value={""} onChange={""}>
+                            <option value="Software Engineer"> Software Engineer</option>
+                            <option value="UI Developer"> UI Developer </option>
+                            <option value="Dev Ops"> Dev Ops</option>
+                        </select>
+                        <input id="SignupPassword" defaultValue={"Password"} {...register("Password", { required: true})} />
+                        <br/>
+                        <input id="SignupPasswordMatch" defaultValue={"Re-enter Password"} {...register("PasswordMatch", { required: true})} />
                         <br/>
                             <div id="submitDiv">
                                 <input id="submitButtonLogin" type="submit" value="Sign Up"/>
