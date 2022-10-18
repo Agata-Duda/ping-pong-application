48571134e9522284c5066535d959225868b9cd6d
Agata-Duda <msagataduda@gmail.com>
2022-10-18 13:20:47 +0100

    Import and Add  DatePicker to Booking-Page

--- src/index.js
+++ src/index.js
@@ -3,12 +3,16 @@ import ReactDOM from "react-dom"
 import "./index.css"
 import App from "./App"
 import AppProvider from "./context/appContext"
+import { LocalizationProvider } from "@mui/x-date-pickers"
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs"
 
 ReactDOM.render(
-  <AppProvider>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  </AppProvider>,
+  <LocalizationProvider dateAdapter={AdapterDayjs}>
+    <AppProvider>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </AppProvider>
+  </LocalizationProvider>,
   document.getElementById("root")
 )
--- src/views/booking-page.js
+++ src/views/booking-page.js
@@ -1,11 +1,13 @@
 import { PageTemplate } from "../templates/PageTemplate"
 import React from "react"
+import DateTimeValidation from "../components/booking/dateTimePicker"
 
 const BookingPage = () => {
   return (
     <PageTemplate>
       <div className="booking">
         <h1> Booking Service</h1>
+        <DateTimeValidation/>
       </div>
     </PageTemplate>
 
