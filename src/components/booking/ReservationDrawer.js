40ffaa2cd49d9fca95c193fbabd168c768e55a36
Agata-Duda <msagataduda@gmail.com>
2022-11-08 16:31:12 +0000

    drawer open on select slot

---
+++ src/components/booking/ReservationDrawer.js
@@ -0,0 +1,9 @@
+import Drawer from "@mui/material/Drawer"
+import * as React from "react"
+
+const ReservationDrawer = () => (
+  <Drawer>
+    <h1> hi </h1>
+  </Drawer>
+)
+export default ReservationDrawer
--- src/components/booking/calendar.js
+++ src/components/booking/calendar.js
@@ -3,34 +3,59 @@ import { Calendar, momentLocalizer } from "react-big-calendar"
 import moment from "moment"
 import "react-big-calendar/lib/css/react-big-calendar.css"
 import axios from "axios"
+import { SettingsPowerRounded } from "@mui/icons-material"
+// import ReservationDrawer from "./ReservationDrawer"
+import Drawer from "@mui/material/Drawer"
+const localizer = momentLocalizer(moment)
 
 const CalendarBooking = () => {
-  const localizer = momentLocalizer(moment)
   const [bookings, setBookings] = useState()
+  const [open, setOpen] = useState(false)
+  const openDrawer = () => {
+    setOpen(true)
+    console.log("hi")
+  }
 
-  useEffect(async () => {
-    const res = await axios("http://localhost:8000/bookings")
-    setBookings(res.data)
+  useEffect(() => {
+    const fetchBookings = async () => {
+      const res = await axios("http://localhost:8000/bookings")
+      setBookings(res.data)
+      const mappedArray = res.data.map(d => ({
+        ...d,
+        eventStart: new Date(d.eventStart),
+        eventEnd: new Date(d.eventEnd)
+      }))
+      setBookings(mappedArray)
+    }
+    fetchBookings()
   }, [])
+  console.log("bookings: ", bookings)
 
-return (
+  return (
     <div>
-     <Calendar onSelectEvent={(e) => {
+      <Calendar onSelectEvent={(e) => {
         console.log(e)
       }}
-      selectable
+      selectable={true}
+      onSelectSlot={(slot) => {
+        openDrawer()
+        console.log("slot select: ", slot)
+      }
+      }
       localizer={localizer}
       events={bookings}
       titleAccessor="id"
-      startAccessor= "eventStart"
-      endAccessor= "eventEnd"
+      startAccessor="eventStart"
+      endAccessor="eventEnd"
       timeslots={1}
       step={5}
       style={{ height: 500 }}
       />
-      <p> Calendar where user can drag to create a slot</p>
       <button> Delete </button>
       <button> Update </button>
+      <Drawer open={open} anchor={"right"} onClose={() => SettingsPowerRounded(false)}>
+      <h1> Open </h1>
+      </Drawer>
     </div>
   )
 }
