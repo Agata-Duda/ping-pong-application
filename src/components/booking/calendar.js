ede58d5bf95d6cc413eec3f03f9336e6adbe2e77
Agata-Duda <msagataduda@gmail.com>
2022-11-08 11:10:49 +0000

    showing bookings with axios from json file

--- src/components/booking/calendar.js
+++ src/components/booking/calendar.js
@@ -1,44 +1,37 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
 import { Calendar, momentLocalizer } from "react-big-calendar"
 import moment from "moment"
 import "react-big-calendar/lib/css/react-big-calendar.css"
-const localizer = momentLocalizer(moment)
+import axios from "axios"
 
-const bookings = [
-  {
-    start: new Date("2022-11-08T06:30:00"),
-    end: new Date("2022-11-08T07:00:00"),
-    title: "titrwwwwwwsrle",
-    userId: "User threwwwe",
-    bookingid: "asdfgdfrwwwwwwwwwwwwwrrrrhjkl"
-  },
-  {
-    start: new Date("2022-10-24T04:30:00"),
-    end: new Date("2022-10-24T05:00:00"),
-    title: "titrrle",
-    userId: "User three",
-    bookingid: "asdfgdfrrrrrhjkl"
-  }
-]
+const CalendarBooking = () => {
+  const localizer = momentLocalizer(moment)
+  const [bookings, setBookings] = useState()
 
-const CalendarBooking = () => (
-  <div>
-    <Calendar onSelectEvent={(e) => {
-      console.log(e)
-    }}
-    selectable
-    localizer={localizer}
-    events={bookings}
-    titleAccessor="bookingid"
-    startAccessor= "start"
-    endAccessor= "end"
-    timeslots={1}
-    step={5}
-    style={{ height: 500 }}
-    />
-    <p> Calendar where user can drag to create a slot</p>
-    <button> Delete </button>
-    <button> Update </button>
-  </div>
-)
+  useEffect(async () => {
+    const res = await axios("http://localhost:8000/bookings")
+    setBookings(res.data)
+  }, [])
+
+return (
+    <div>
+     <Calendar onSelectEvent={(e) => {
+        console.log(e)
+      }}
+      selectable
+      localizer={localizer}
+      events={bookings}
+      titleAccessor="id"
+      startAccessor= "eventStart"
+      endAccessor= "eventEnd"
+      timeslots={1}
+      step={5}
+      style={{ height: 500 }}
+      />
+      <p> Calendar where user can drag to create a slot</p>
+      <button> Delete </button>
+      <button> Update </button>
+    </div>
+  )
+}
 export default CalendarBooking
