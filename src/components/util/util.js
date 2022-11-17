2d7f4b44c6eec7bbc244af178cfb94588e032919
PatrickMcKiernan <Patrick.McKiernan@Zinkwoks.com>
2022-11-17 17:12:01 +0000

    Implementing Create tournament

--- src/components/util/util.js
+++ src/components/util/util.js
@@ -5,6 +5,7 @@ export const GetAllReservations_URL = "http://localhost:8120/reservations";
 export const Reservation_URL = "http://localhost:8120/reservations";
 export const BASE_URL = "http://localhost:8000/bookings";
 export const GET_ALL_USERS = "http://localhost:8110/users";
+export const Tournament_URL = "http://localhost:8130/tournament";
 
 export const routes = {
   loginpage: "/login-page",
@@ -70,3 +71,7 @@ export const updateReservationById = async (id) => {
 export const deleteReservationById = async (id) => {
   await axios.delete(`${Reservation_URL}/"${id}`);
 };
+
+export const postTournament = async (tournament) => {
+  await axios.post(`${Tournament_URL}/`, tournament)
+}
