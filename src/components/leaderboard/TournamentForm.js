4eb2ca48b03059687bda2d4f5223d6a70000d63a
PatrickMcKiernan <Patrick.McKiernan@Zinkwoks.com>
2022-11-17 17:13:30 +0000

    Implenting Create Torunament

---
+++ src/components/leaderboard/TournamentForm.js
@@ -0,0 +1,51 @@
+import React from "react"
+import { v4 } from "uuid"
+import Collapsible from "react-collapsible"
+import { postTournament } from "../util/util"
+import { useForm } from "react-hook-form"
+
+const CreateTournamentForm = () =>{
+    const { register, handleSubmit, reset} = useForm()
+    const current = new Date()
+    console.log(current)
+    const id = v4()
+
+
+    const onSubmitTournament = (tournament) => {
+        postTournament(
+            {
+                tournamentId: id,
+                tournamentName: tournament.tournament_name,
+                tournamentType: tournament.tournamentType,
+                createdOn: current,
+                updatedOn: current,
+                endedOn: tournament.ended_on
+            })
+        reset()
+    }
+    return(
+         <Collapsible trigger={<b>Create Tournament</b>} triggerWhenOpen={<b>Create</b>}>
+            <div id="formCreateTournament" className="TournamentForm">
+                <form onSubmit={handleSubmit(onSubmitTournament)}>
+                <input id="Tournamentname" defaultValue={"Tournament Name"} {...register("tournament_name", { "required": true })} />
+                <br/>
+                <label> Select Tournament Type </label>
+                <select {...register("tournamentType")}>
+                    <option value="Knock Out"> Knock Out</option>
+                    <option value="Practice"> Practice </option>
+                    <option value="League"> League</option>
+                    <option value="Group Stage"> Group Stage</option>
+                </select><br/>
+                <label>Select End Date</label>
+                <input type="datetime-local" {...register("ended_on", {"required": true})}/>
+                <br/>
+                <div id="submitDiv">
+                    <input id="submitCreateTournament" type="submit" value="Create a Tournament"/>
+                </div>
+                </form>
+            </div>
+         </Collapsible>
+    )
+}
+
+export default CreateTournamentForm
