dedb7a5072ae2a4ff4f29deb7c5336606db8ab45
Agata-Duda <msagataduda@gmail.com>
2022-11-22 14:13:01 +0000

    Added page refresh after form submit, error handle

--- src/components/reservation/CreateReservationForm.js
+++ src/components/reservation/CreateReservationForm.js
@@ -1,19 +1,22 @@
 import React, { useState, useEffect } from "react"
 import axios from "axios"
 import Select from "react-select"
-import { useForm } from "react-hook-form"
+import { useForm, Controller } from "react-hook-form"
 import { v4 } from "uuid"
+import toast from "react-hot-toast"
 
 import { GET_ALL_TOURNAMENTS_URL, GET_ALL_USERS, postBooking } from "../util/ApiMethods"
-//ME: Add toast to on submit form and add validation to form from example at end of file 
+
 export default function CreateReservationForm ({ timeDate, closeDrawer }) {
+  
   const [ userNames, setUserName ] = useState([]);
   const [ tournaments, setTournaments ] = useState([]);
-  const { register, handleSubmit, formState: { errors } } = useForm()
+  const { control, handleSubmit, formState: { errors } } = useForm()
   const [ selectedPlayerOne, setSelectedPlayerOne ] = useState(null)
   const [ selectedPlayerTwo, setSelectedPlayerTwo ] = useState(null)
   const [ selectedTournament, setSelectedTournament ] = useState(null)
   const [ selectedSets, setSelectedSets ] = useState(null)
+
   const setsOptions = [
     { value: "1", label: "1"},
     { value: "3", label: "3"},
@@ -63,57 +66,94 @@ export default function CreateReservationForm ({ timeDate, closeDrawer }) {
         tournament_id: selectedTournament.value
       })
         closeDrawer()
-      //TODO: add toast
+      toast.success("Reservation Created Successfully!")
+      window.location.reload();
       }
 
   return (
     <>
     <form onSubmit={handleSubmit(onSubmitReservation)}>
       <label> Start Time : </label>
-      <label {...register("event_start")}> {new Date(timeDate.start).toString()}</label> <br/>
+      <label> {new Date(timeDate.start).toString()}</label> <br/>
       <label> End Time : </label>
-      <label {...register("event_finish")}> {new Date(timeDate.end).toString()}</label> <br/>
+      <label> {new Date(timeDate.end).toString()}</label> <br/>
 
       <label> Select Player One </label>
-      <Select {...register("player_1", {required : true})}
-      aria-invalid={errors.player_1 ? "true" : "false"} options={userNames} onChange={setSelectedPlayerOne} defaultValue={selectedPlayerOne} placeholder="Select Player one" />
-      {errors.player_1?.type === 'required' && <p role="alert">Player One is required</p>}
+      <Controller 
+        control={control}
+        name="SelectPlayerOne"
+        render={({ field }) => 
+        <Select 
+          {...field}
+          options={userNames}
+          onChange={setSelectedPlayerOne}
+          defaultValue={selectedPlayerOne}
+          placeholder="Select Player One"
+          required
+          error={!!errors.SelectPlayerOne}
+          helperText={errors.SelectPlayerOne && `${errors.SelectPlayerOne.message}`}
+        />
+      
+      }
+      />
        <br/>
-
       <label> Select Player Two </label>
-      <Select {...register("player_2", {required : true})} options={userNames} onChange={setSelectedPlayerTwo} defaultValue={selectedPlayerTwo} placeholder="Select Player Two"
-      aria-invalid={errors.player_2 ? "true" : "false"} />
-      {errors.player_1?.type === 'required' && <p role="alert">Player Two is required</p>}
-        <br/>
-
-      <label> Select Tournament </label>
-      {/* TODO Tip: at the end of this file you can find useful validation, you can use it*/}
-      <Select {...register("tournament_name",{required : true})} options={tournaments} onChange={setSelectedTournament} defaultValue={selectedTournament} placeholder="Select Tournament"
-      aria-invalid={errors.tournament_name ? "true" : "false"} />
-      {errors.tournament_name?.type === 'required' && <p role="alert">Tournament name is required</p>}
+      <Controller 
+        control={control}
+        name="SelectPlayerTwo"
+        render={({ field }) => 
+        <Select 
+          {...field}
+          label="Select Player Two"
+          options={userNames}
+          onChange={setSelectedPlayerTwo}
+          defaultValue={selectedPlayerTwo} 
+          placeholder="Select Player Two"
+          required
+          error={!!errors.SelectPlayerTwo}
+          helperText={errors.SelectPlayerTwo && `${errors.SelectPlayerTwo.message}`}
+        />
+      }
+      />
       <br/>
-
+      <label> Select Tournament </label>
+      <Controller 
+        control={control}
+        name="SelectTournamentName"
+        render={({ field }) => 
+        <Select 
+          {...field}
+          options={tournaments}
+          onChange={setSelectedTournament}
+          defaultValue={selectedTournament} 
+          placeholder="Select Tournament"
+          required
+          error={!!errors.SelectTournamentName}
+          helperText={errors.SelectTournamentName && `${errors.SelectTournamentName.message}`}
+        />
+      }
+      />
+      <br/>
       <label> Select Set Number </label>
-      <Select {...register("sets", {required : true})} options={setsOptions} onChange={setSelectedSets} defaultValue={selectedTournament} placeholder="Select Sets"
-      aria-invalid={errors.sets ? "true" : "false"} />
-      {errors.sets?.type === 'required' && <p role="alert">Sets is required</p>}
+      <Controller 
+        control={control}
+        name="SelectSets"
+        render={({ field }) => 
+        <Select 
+          {...field}
+          options={setsOptions}
+          onChange={setSelectedSets}
+          defaultValue={setSelectedSets} 
+          placeholder="Select Set"
+          required
+          error={!!errors.SelectSets}
+          helperText={errors.SelectSets && `${errors.SelectSets.message}`}
+        />
+      }
+      />
       <br/>
       <button type="submit"> Submit </button>
     </form>
     </>
   )
 }
-
-
-//TODO Validation:
-// {...register("tournament_name", {
-//   required: "Tournament name is required",
-//   minLength: {
-//     value: 5,
-//     message: "Comment must be at least 5 characters",
-//   },
-//   maxLength: {
-//     value: 50,
-//     message: "Comment must be under 50 characters",
-//   },
-// })}
