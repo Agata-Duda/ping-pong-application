9cfe7f9e20821021462acc01bebd445c84a44749
PatrickMcKiernan22 <114994868+PatrickMcKiernan22@users.noreply.github.com>
2022-11-29 15:12:51 +0000

    Added new input value for create tournament form

--- src/components/leaderboard/CreateTournamentForm.js
+++ src/components/leaderboard/CreateTournamentForm.js
@@ -6,6 +6,10 @@ import { v4 } from "uuid"
 import { Box } from "@mui/material"
 
 import { postTournament } from "../util/ApiMethods"
+
+const labelStyle = { 
+    paddingLeft: 40
+}
  
 const CreateTournamentForm = () => {
     const { register, handleSubmit, reset} = useForm()
@@ -20,14 +24,15 @@ const CreateTournamentForm = () => {
                 tournamentType: tournament.tournamentType,
                 createdOn: current,
                 updatedOn: current,
+                startDate: tournament.startDate,
                 endedOn: tournament.ended_on
             })
         reset()
     }
     return(
         //TODO: change style later 
-         <Collapsible trigger={<b>Create Tournament</b>} triggerWhenOpen={<b>Create</b>}>
-            <Box>
+         <Collapsible trigger={<b style={labelStyle}>Create Tournament</b>} triggerWhenOpen={<b style={labelStyle}>Create</b>}>
+            <Box sx={{paddingLeft:5}}>
                 <form onSubmit={handleSubmit(onSubmitTournament)}>
                     <input defaultValue={"Tournament Name"} {...register("tournament_name", { "required": true })} />
                     <br/>
@@ -38,6 +43,9 @@ const CreateTournamentForm = () => {
                         <option value="League"> League</option>
                         <option value="Group Stage"> Group Stage</option>
                     </select>
+                    <br/>
+                    <label>Select Start Date</label>
+                    <input type="datetime-local" {...register("start_date", {"required": true})}/>
                     <br/>
                     <label>Select End Date</label>
                     <input type="datetime-local" {...register("ended_on", {"required": true})}/>
