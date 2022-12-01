7ca23e9ffb4efac81a0e5266172963f844117e99
Agata-Duda <msagataduda@gmail.com>
2022-12-01 09:51:00 +0000

    impl table component for leaderboard page

--- src/views/LeaderboardView.js
+++ src/views/LeaderboardView.js
@@ -2,10 +2,12 @@ import React from "react"
 
 import { PageTemplate } from "../templates/PageTemplate"
 import CreateTournamentForm from "../components/leaderboard/CreateTournamentForm"
+import  LeaderboardTable  from "../components/leaderboard/LeaderboardTable"
 
 const LeaderboardView = () => (
   <PageTemplate>
       <CreateTournamentForm/>
+      <LeaderboardTable/>
   </PageTemplate>
 )
 
